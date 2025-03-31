0dfa64ccc98b0f0ef594f0a3ec7197b21a8be53d
aarestigue <andrea.arestigue@gmail.com>
2025-03-31 22:10:44 +0000

    Added new get rout for the Update Form

--- index.js
+++ index.js
@@ -34,12 +34,42 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
+
 // * Code for Route 2 goes here
+app.get('/update-trainings', async (req, res) => {
+    try {
+        res.render('updates', { title: 'Trainings Form' });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
 
+app.post('/update-trainings', async (req, res) => {
+    const update = {
+        properties: {
+            "favorite_book": req.body.newVal
+        }
+    }
+
+    const email = req.query.email;
+    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.patch(updateContact, update, { headers } );
+        res.redirect('back');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
